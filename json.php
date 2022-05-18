4c7b138ca25f5fe792a05f57fe8d0f8cc253e71d
Habib-4-Tech <rudra.habib@gmail.com>
2022-05-18 22:23:05 +0600

    Another story API created (XML)  Phase-4 completed

--- json.php
+++ json.php
@@ -8,6 +8,8 @@ include('db.php')
 
 <?php
 
+$arrerr= array('error'=>"No Data Found!");
+
 if (isset($_GET['Story_id']))
 {
 $Story_id=$_GET['Story_id'];
@@ -15,6 +17,10 @@ $Story_id=$_GET['Story_id'];
 $sql= "Select *  FROM story where id= $Story_id";
 
 $result = mysqli_query($connection, $sql);
+
+
+if ($result && mysqli_num_rows($result) > 0)
+{
 $result = mysqli_fetch_assoc($result);
 $N_title= $result['title'];
 $N_text= $result['body'];
@@ -25,9 +31,18 @@ $N_date=$result['str_date'];
 $arr = array('title' => $N_title , 'body' => $N_text, 'published_date' => $N_date);
 
 echo json_encode($arr);
-
+}
+else
+{
+ echo json_encode($arrerr);
 }
 
+
+
+}
+else{
+    echo json_encode($arrerr);
+}
 
 
 
--- story_listing.php
+++ story_listing.php
@@ -1,53 +1,68 @@
-
-<?php 
+<?php
 include('db.php');
 
 ?>
 
 <?php
-$results_per_page=4;
+$results_per_page = 4;
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <title>Stories Listing Page</title>
-
-  
-    <style>
- 
- .button {
-  background-color: #4CAF50; /* Green */
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 10px;
-  margin: 4px 2px;
-  cursor: pointer;
-  -webkit-transition-duration: 0.4s; /* Safari */
-  transition-duration: 0.4s;
-}
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <title>Stories Listing Page</title>
 
 
-.button:hover {
-  box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24),0 17px 50px 0 rgba(0,0,0,0.19);
-}
+  <style>
+    .button {
+      background-color: black;
+      border: 2px solid transparent;
+      border-radius: 4px;
+      color: white;
+      padding: 7px 16px;
+      text-align: center;
+      text-decoration: none;
+      display: inline-block;
+      font-size: 15px;
+      margin: 4px 2px;
+      cursor: pointer;
+      font-weight: 600;
+      -webkit-transition-duration: 0.1s;
+      transition-duration: 0.1s;
+
+    }
 
 
-.split-para      { display:block;margin:10px;}
-.split-para span { display:block;float:right;width:20%;margin-left:10px;}
-</style>
+    .button:hover {
+      border: 2px solid black;
+      color: black;
+      text-transform: none;
+      background-color: white;
+    }
+
+
+    .split-para {
+      display: block;
+      margin: 10px;
+    }
+
+    .split-para span {
+      display: block;
+      float: right;
+      width: 20%;
+      margin-left: 10px;
+    }
+  </style>
 
 </head>
+
 <body>
 
 
@@ -68,108 +83,112 @@ $results_per_page=4;
 
 
 
-  <div class="conatiner" style="text-align:center;padding:50px;"><h1><b> Stories Listing </b> </h1></div>  
-
-  <?php 
- if (isset($_GET["page"])) { 
-     $page  = $_GET["page"];
-     } 
- else {
-      $page=1; 
-    };
- $start_from = ($page-1) * $results_per_page;
- $sql = "SELECT * FROM story ORDER BY p_date DESC LIMIT $start_from, ".$results_per_page;
- $result = mysqli_query($connection, $sql);
- ?>
-
-
-
- <?php 
-    foreach($result as $key => $value) {
-     ?>
-
-<div class="card w-80" style="padding: 40px; margin: 0px 0px 40px 40px ;">
-  <div class="card-body">
-    <h3 class="card-title"><?=  $value['title']?></h3>
-    <h5  class="card-text">
-  
-     By <?=  $value['user_id']?>  on <?=  $value['str_date']?> 
-
-    </h5>
-
-
-
-    <p class="card-text"><?=  $value['body']?></p>
-
-
-
-
-
-
-    <form action="delete.php" method="post">
-
-    <p class="split-para">       <a href="json.php?Story_id=<?= $value['id'] ?>" class="button" >JSON</a>
-    <a href="#" class="button">Button</a>
-    
-    <span>
-    <a href="edit.php?Story_id=<?= $value['id'] ?>" class="button" >Edit</a>
-    
-  
-
-            <button type="submit" class="button" name="submit" >Delete</button>
-          </right>
-            <input type="text" class="form-control" id="Story_id"  name="Story_id" value='<?= $value['id']?>' hidden >
-             
-
-</span></p>
-
-
-</form>
-
-
-    
-
+  <div class="conatiner" style="text-align:center;padding:50px;">
+    <h1><b> Stories Listing </b> </h1>
   </div>
-</div>
 
-<?php
+  <?php
+  if (isset($_GET["page"])) {
+    $page  = $_GET["page"];
+  } else {
+    $page = 1;
+  };
+  $start_from = ($page - 1) * $results_per_page;
+  $sql = "SELECT * FROM story ORDER BY p_date DESC LIMIT $start_from, " . $results_per_page;
+  $result = mysqli_query($connection, $sql);
+  ?>
+
+
+
+  <?php
+  foreach ($result as $key => $value) {
+  ?>
+
+    <div class="card " style="padding: 40px; margin: auto ; width:80%; border: 0%; box-shadow:0 0 0 0;">
+      <div class="card-body">
+        <h3 class="card-title"><?= $value['title'] ?></h3>
+        <h5 class="card-text">
+
+          By <?= $value['user_id'] ?> on <?= $value['str_date'] ?>
+
+        </h5>
+
+
+
+        <p class="card-text"><?= $value['body'] ?></p>
+
+
+
+
+
+
+        <form action="delete.php" method="post">
+
+          <p class="split-para">
+
+          <a href="xml.php?Story_id=<?= $value['id'] ?>" class="button">XML</a>
+
+          <a href="json.php?Story_id=<?= $value['id'] ?>" class="button">JSON</a>
+           
+
+            <span>
+              <a href="edit.php?Story_id=<?= $value['id'] ?>" class="button">Edit</a>
+
+
+
+              <button type="submit" class="button" name="submit">Delete</button>
+              </right>
+              <input type="text" class="form-control" id="Story_id" name="Story_id" value='<?= $value['id'] ?>' hidden>
+
+
+            </span>
+          </p>
+
+
+        </form>
+
+
+
+
+      </div>
+    </div>
+    <br>
+  <?php
+  }
+  ?>
+
+
+  <center>
+
+    <?php
+    $sql = "SELECT COUNT(id) AS total FROM story";
+
+    $result = mysqli_query($connection, $sql);
+    $result = mysqli_fetch_assoc($result);
+    $total_pages = ceil($result["total"] / $results_per_page); // calculate total pages with results
+
+
+    if ($page > 1) {
+      echo "<a href='story_listing.php?page=" . ($page - 1) . "'class='btn btn-dark'> << </a>";
     }
+
+
+    for ($i = 1; $i <= $total_pages; $i++) {  // print links for all pages
+      echo "<a href='story_listing.php?page=" . $i . "' class='btn btn-dark'";
+      if ($i == $page)  echo " class='curPage'";
+      echo ">" . $i . "</a> ";
+    }
+
+
+
+    if ($page < $total_pages) {
+      if ($i > $page) {
+        echo "<a href='story_listing.php?page=" . ($page + 1) . "'class='btn btn-dark'> >> </a>";
+      }
+    }
+
     ?>
-
-
-<center>
-
- <?php
- $sql = "SELECT COUNT(id) AS total FROM story";
-
- $result = mysqli_query($connection, $sql);
-$result= mysqli_fetch_assoc($result);
- $total_pages = ceil($result["total"] / $results_per_page); // calculate total pages with results
-   
-
-if ($page>1)
-{
-    echo "<a href='story_listing?page=".($page-1)."'class='btn btn-danger'> << </a>";
-}
-
-
- for ($i=1; $i<=$total_pages; $i++) {  // print links for all pages
-             echo "<a href='story_listing.php?page=".$i."' class='btn btn-primary'";
-             if ($i==$page)  echo " class='curPage'";
-             echo ">".$i."</a> ";
- }
-
-
- 
-if($page<$total_pages){
- if ($i>$page)
- {
-     echo "<a href='story_listing.php?page=".($page+1)."'class='btn btn-danger'> >> </a>";
- }
-}
-
- ?>
-</center>
+  </center>
 
 
 
@@ -184,4 +203,5 @@ if($page<$total_pages){
 
 
 </body>
+
 </html>
