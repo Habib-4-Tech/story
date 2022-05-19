937c56cd4ce8756d490f798309f729f49fb4f42f
Habib-4-Tech <rudra.habib@gmail.com>
2022-05-19 23:41:51 +0600

    created login page

---
+++ login.php
@@ -0,0 +1,193 @@
+<?php
+session_start();
+
+include('db.php');
+
+
+
+$id = $Password = $User = "";
+
+$idErr = $PasswordErr = "";
+
+$FlagError = 0;
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+
+
+    if (empty($_POST["id"])) {
+        $idErr = "User ID is required";
+        $FlagError = 1;
+    } else {
+        $id = test_input($_POST["id"]);
+
+        $sql = "select * from user_data where user_id = '$id'";
+        $result = mysqli_query($connection, $sql);
+
+        if ($result && mysqli_num_rows($result) > 0) {
+
+            $User = mysqli_fetch_assoc($result);
+
+            if ($User['user_id'] != $id) {
+                $idErr = "User ID doesn't Match";
+                $FlagError = 1;
+            }
+        } else {
+            $idErr =  "User ID does not exist";
+            $FlagError = 1;
+        }
+    }
+
+    if (empty($_POST["password"])) {
+        $PasswordErr = "password is required";
+        $FlagError = 1;
+    } else {
+        $Password = test_input($_POST["password"]);
+
+        if ($User['password'] != $Password) {
+            $PasswordErr = "Password is not Matched";
+            $FlagError = 1;
+        }
+    }
+
+
+
+
+    if ($FlagError == 0) {
+
+
+        if (isset($_POST['submit'])) {
+
+            $_SESSION['USER_DATA'] = [
+                'ID' => $User['user_id'],
+                'NAME' => $User['full_ame'],
+
+            ];
+
+            if (isset($_POST['remember_me'])) {
+
+
+                setcookie('user_id', $User['user_id'], time() + (24 * 60 * 60), "/");
+            }
+
+            mysqli_close($connection);
+
+
+            header("Location:story_listing.php");
+        }
+    }
+}
+
+
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+
+?>
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Register</title>
+
+    <style>
+        span {
+            color: red;
+        }
+
+        .button {
+            background-color: black;
+            /* Green */
+            border: none;
+            color: white;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            margin: 4px 2px;
+            cursor: pointer;
+            -webkit-transition-duration: 0.4s;
+            /* Safari */
+            transition-duration: 0.4s;
+        }
+
+
+
+        .button:hover {
+            box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24), 0 17px 50px 0 rgba(0, 0, 0, 0.19);
+        }
+    </style>
+</head>
+
+
+
+
+<body>
+
+
+    <div class="card " style="padding: 40px; margin: auto ; width:70%; border: 0%; box-shadow:0 0 0 0;">
+
+        <h1 align=center><b> User Registration </b></h1>
+
+
+        <form action="login.php" method="post">
+
+            <div class="form-group">
+                <label for="id">User ID</label>
+                <input type="text" class="form-control" id="id" name="id"  
+                <?php
+                
+                  if(isset($_COOKIE['user_id'])) {   ?>  value="  <?php         echo $_COOKIE['user_id']; } ?>"  
+                  <?php
+                  if(!isset($_COOKIE['user_id'])) {   ?>    placeholder="Enter your user ID"
+
+                  <?php } ?>
+
+                     >
+                <span> * <?php echo $idErr ?></span>
+            </div>
+
+
+
+
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" name="password" id="password" placeholder="Enter your password">
+
+                <span> * <?php echo $PasswordErr ?></span>
+            </div>
+
+            <div class="form-group">
+                <center>
+                    <button type="submit" name='submit' class="button">Submit</button>
+
+                    <input type="checkbox" name="remember_me" value="yes">
+                    <label>Remember Me</label><br>
+                </center>
+            </div>
+
+
+        </form>
+
+    </div>
+
+
+</body>
+
+</html>
